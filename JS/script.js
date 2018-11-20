d34999cdee504c5b12f71e9cffcfbd1d71fa8ba2
donaldduck234 <45213617+donaldduck234@users.noreply.github.com>
2018-11-20 20:56:11 +0000

    inizio
    inizio

--- JS/script.js
+++ JS/script.js
@@ -1,74 +1,34 @@
 //VARIABLES
 const display = document.getElementById('display');
+const ongoingCalculation = document.getElementById('ongoing-calculation');
 const numbers = document.getElementsByClassName('numbers');
-const keyboard = document.getElementById('keyboard');
-let ongoingCalculation = document.getElementById('ongoing-calculation');
-//BOOLEAN VALUES
-let oper = false;
-let num = false;
-let equal;
-let point = false;
-let ac;
+let equalSwitch;
+
 
 //EVENT HANDLER
 keyboard.addEventListener('click', (e) => {
-    //            NUMBERS
-    if (e.target.className == 'numbers') {
-        if (display.innerHTML === 0) {
-            display.innerHTML = e.target.innerHTML;
-            ongoingCalculation.innerHTML = e.target.innerHTML;
-            oper = false;
-            num = true;
-        } else if (display.innerHTML !== 0) {
-            if (e.target.className == 'numbers') {
-                display.innerHTML += e.target.innerHTML;
-                ongoingCalculation.innerHTML += e.target.innerHTML;
-            }
-            oper = false;
-            num = true;
-        }
-
-        //POINT
-        //            if (!point) {
-        //                if (e.target.className == 'point') {
-        //                    display.innerHTML += e.target.innerHTML;
-        //                    oper = false;
-        //                    num = true;
-        //                    point = true;
-        //                    display.innerHTML = '0.';
-        //                    point = true;
-        //                } else if (point) {
-        //                    if (e.target.className == 'point') {
-        //                        display.innerHTML += '';
-        //                    }
-        //                }
-
-        //OPERATORS
-        if (e.target.className == 'operators') {
-            if (oper === false) {
-                display.innerHTML += e.target.innerHTML;
-                ongoingCalculation.innerHTML += e.target.innerHTML;
-                oper = true;
-                num = false;
-            } else if (oper) {
-                display.innerHTML += '';
-                oper = true;
-                num = false;
-            }
-        }
-        //HELPERS
-        if (e.target.className == 'helpers ac') {
-            display.innerHTML = 0;
-        }
-        if (!num) {
-            if (e.target.className == 'numbers zero') {
-                display.innerHTML += '';
-            }
-        }
-        //RESULT
-        if (e.target.className == 'equal') {
-            display.innerHTML = eval(display.innerHTML);
-            ongoingCalculation.innerHTML += eval(display.innerHTML);
-        }
+    if (e.target.className === 'numbers') {
+        display.innerHTML += e.target.innerHTML;
+        ongoingCalculation.innerHTML += e.target.innerHTML;
+    } else if (e.target.className === 'operators') {
+        display.innerHTML = 0;
+        ongoingCalculation.innerHTML += e.target.innerHTML;
+        console.log(ongoingCalculation);
+    } else if (e.target.className === 'helpers ac') {
+        display.innerHTML = 0;
+        ongoingCalculation.innerHTML = 0;
+    } else if (e.target.className === 'equal') {
+        display.innerHTML = eval(ongoingCalculation.innerHTML);
+        ongoingCalculation.innerHTML = eval(ongoingCalculation.innerHTML);
+        equalSwitch = true;
     }
+
+    if (equalSwitch) {
+        display.innerHTML = 0;
+        ongoingCalculation.innerHTML = 0;
+        display.innerHTML += e.target.innerHTML;
+        ongoingCalculation.innerHTML += e.target.innerHTML;
+    }
+
+
 });
